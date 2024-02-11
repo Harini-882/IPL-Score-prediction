ddd838558cacc7a7875fff31a3c996ff9be33c62
Harini G <128168902+Harini-882@users.noreply.github.com>
2024-02-11 20:01:12 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+          import pandas as pd
+          import matplotlib
+          import matplotlib.pyplot as plt
+          import numpy as np
+          import pylab
+          from datetime import datetime
+          from statistics import mode
+          from mpl_toolkits.basemap import Basemap
+          "\n"
+          "#%matplotlib inline\n"
+          pylab.rcParams['figure.figsize'] = (10, 8)
+        ]
+      }
+      {
+        cell_type: "code"
+        execution_count: null
+        metadata: {
+          id: "z1cWXUMp1OYr",
+          outputId": "f06c1232-ae98-47fc-ed2f-247077db1e06"
+        },
+        outputs": [
+          {
+            "data": {
